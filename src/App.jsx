1c8f7ddad1b838c9c3b7f928aaaad2a49ca068c1
jaikrishnaverma <jaikrishnaverma>
2022-10-12 11:26:33 +0530

    dsf

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <div className="div1">
+    <nav className='row'><img src="image__6_-removebg-preview.png" alt="" className="row1" /> <div className="row2"><button className="btn1">SUBSCRIBE</button>or <button className="btn2">LOGIN</button></div></nav>
+    <img src="Desktop.png" alt="" />
+  </div>
+  </>
+  );
+}
+
+export default App;
